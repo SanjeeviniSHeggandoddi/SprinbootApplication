716bf405f380c5e315e8168aa955cf1c5a15d3bd
SanjeeviniSHeggandoddi <sanjeevini.sh@wowonii.com>
2023-06-27 11:38:40 +0530

    Created objects for User

--- src/main/java/com/assignment/springbootapplication/SpringbootapplicationApplication.java
+++ src/main/java/com/assignment/springbootapplication/SpringbootapplicationApplication.java
@@ -1,5 +1,6 @@
 package com.assignment.springbootapplication;
 
+import com.assignment.springbootapplication.entity.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,5 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootapplicationApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootapplicationApplication.class, args);
+		User userDetails1=new User();
+		User userDetails2=new User();
+		System.out.println(userDetails1.hashCode());
+		System.out.println(userDetails2.hashCode());
 	}
 }
