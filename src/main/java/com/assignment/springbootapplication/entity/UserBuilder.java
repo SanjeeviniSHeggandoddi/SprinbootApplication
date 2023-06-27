4cdf2da2ccbfee7afba4a92c18a9d47ec7608197
SanjeeviniSHeggandoddi <sanjeevini.sh@wowonii.com>
2023-06-27 12:22:56 +0530

    Created UserBuilder.java

---
+++ src/main/java/com/assignment/springbootapplication/entity/UserBuilder.java
@@ -0,0 +1,34 @@
+package com.assignment.springbootapplication.entity;
+
+import java.util.Date;
+
+public class UserBuilder {
+    private int id;
+    private String name;
+    private String email;
+    private Date dob;
+
+    public UserBuilder setId(int id) {
+        this.id = id;
+        return  this;
+    }
+
+    public UserBuilder setEmail(String email) {
+        this.email = email;
+        return  this;
+    }
+
+    public UserBuilder setDob(Date dob) {
+        this.dob = dob;
+        return  this;
+    }
+
+    public UserBuilder setName(String name) {
+        this.name = name;
+        return  this;
+    }
+    public User getUser(){
+        return new User(id,name,email,dob);
+    }
+
+}
